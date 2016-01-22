05c921be1d1b2bef855a540c4a94f4ac2aff44ce
rwoolive <rwoolive@utk.edu>
2016-01-22 14:49:07 -0500

    completed exercise 2

--- R/exercise.R
+++ R/exercise.R
@@ -10,8 +10,8 @@ GetFromTreeBase_exercise <- function() {
 	# Replace the "__________" with appropriate arguments
 	# When you run this, it'll take some time. But less time than sequencing
 	# and building the tree yourself.
-	trees <- search_treebase(input="__________", by="__________", 
-	branch_lengths="__________")
+	trees <- search_treebase(input="Formica", by="taxon", 
+	branch_lengths=TRUE)
 	
 	# Pull out just the biggest tree (for convenience)
 	biggest.tree <- trees[[which.max(sapply(trees, Ntip))]]
@@ -41,14 +41,14 @@ GetTreeFromOpenTree_exercise <- function() {
 
 	# Replace the "__________" with appropriate arguments
 	
-	anolis.id <- tnrs_match_names("__________")$ott_id
+	anolis.id <- tnrs_match_names("Anolis")$ott_id
 	
 	# Now get Open Tree's current best estimate of the phylogeny for the group
 	# They call this the tree of life; we can get the subtree for just this group.
 	anolis.tree <- tol_subtree(ott_id=anolis.id)
 	
 	# Let's plot the tree:
-	plot.phylo("__________", type="fan", cex=0.2)
+	plot.phylo(anolis.tree, type="fan", cex=0.2)
 	
 	# It has a lot of polytomies, representing uncertainty. A maximally resolved
 	# tree (if rooted) will have one fewer internal nodes than terminal nodes:
@@ -71,13 +71,13 @@ GetTreeFromOpenTree_exercise <- function() {
 	anolis.studies.ids <- unlist(anolis.studies$matched_studies)
 	
 	# Let's get info on the first study
-	anolis.study1.metadata <- get_study_meta(anolis.studies[[1]][[1]]$`ot:studyId`)
+	anolis.study1.metadata <- get_study_meta(anolis.studies[[1]][[1]])
 	print(get_publication(anolis.study1.metadata))
 	
 	# And let's get the tree from this study
 	# Look in rotl documentation for the right function
 	# Hint: get_study_trees() is close, but you don't know the tree.id
-	anolis.study1.tree1 <- get_______FUNCTION_FROM_rotl____(anolis.studies.ids[[1]])
+	anolis.study1.tree1 <- get_study_tree(anolis.studies[[1]][[1]], tree= anolis.studies[[3]])
 	
 	# And plot it
 	plot.phylo(anolis.study1.tree1, type="fan", cex=0.2)
